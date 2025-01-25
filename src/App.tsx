17544043ec156725a881ecf76669f9cec0c94954
pavvelpotapov <pavelminsk1979@mail.ru>
2025-01-25 03:34:52 +0300

    first commit+

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import {GoogleButton} from "./GoogleButton";
 
 function App() {
   return (
     <div className="App">
- OAUTH2 TRAIN
+ <GoogleButton/>
     </div>
   );
 }
---
+++ src/GoogleButton.tsx
@@ -0,0 +1,16 @@
+export const GoogleButton = () => {
+    const handleLogin = () => {
+        const CLIENT_ID = '924707972646-299oa203pbkhdc6rv65gpvkliqlbkhve.apps.googleusercontent.com'
+        const REDIRECT_URL = 'http://localhost:3000/auth/google-redirect'
+        const scope = 'email profile'
+        const url = `https://accounts.google.com/o/oauth2/v2/auth?scope=${scope}&response_type=code&redirect_uri=${REDIRECT_URL}&client_id=${CLIENT_ID}`
+        window.location.assign(url)
+
+    }
+    return (
+        <div>
+            <button onClick={handleLogin}>GoogleButton</button>
+
+        </div>
+    )
+}
